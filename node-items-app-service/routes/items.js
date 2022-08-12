98ee42796f4a64ddf9aeabedc7d380ba607fa48a
satyajitnayakvc <110887391+satyajitnayakvc@users.noreply.github.com>
2022-08-12 14:05:06 +0530

    Update items.js

--- node-items-app-service/routes/items.js
+++ node-items-app-service/routes/items.js
@@ -9,6 +9,7 @@ const data = [
     { id: 3, title: 'Write new article', order: 3, completed: true, createdOn: new Date() },
     { id: 4, title: 'Walk toward home', order: 4, completed: false, createdOn: new Date() },
     { id: 5, title: 'Have some dinner', order: 5, completed: false, createdOn: new Date() },
+     { id: 6, title: 'Have cup of tea', order: 6, completed: false, createdOn: new Date() },
 ];
 
 // READ
